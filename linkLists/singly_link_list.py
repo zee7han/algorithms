96044620b08526d78f92723622bb8bdf456c52cf
Mohammad Zeeshan <zee7han@gmail.com>
2018-07-27 12:32:39 +0530

    Add the implementation for singly link list node

---
+++ linkLists/singly_link_list.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.nextNode = b
+b.nextNode = c
+
+print(a.value)
+print(a.nextNode.value)
+print(b.value)
+print(b.nextNode.value)
